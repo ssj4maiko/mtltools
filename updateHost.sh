61442bd7b2590d285fb48a62c374182b56e95d95
Maiko Gabriel Kinzel Engelke <ssj4maiko@gmail.com>
2019-06-19 09:00:04 -0400

    Migrate...

--- updateHost.sh
+++ updateHost.sh
@@ -2,7 +2,7 @@ git reset --hard origin/master
 git pull
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
-php artisan make:migration
+php artisan migrate
 php artisan cache:clear
 php artisan route:cache  
 php artisan config:cache
