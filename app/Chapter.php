a876a67f74d5e0ddb5a2fbc24d1f4a7e1a07483c
Maiko Gabriel Kinzel Engelke <ssj4maiko@gmail.com>
2019-07-15 10:35:30 -0400

    Fix from updating every X chapter in all numbers upon updates

--- app/Chapter.php
+++ app/Chapter.php
@@ -3,14 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Chapter extends Model
 {
 	//
 	protected $table = 'chapters';
-	protected $primaryKey = 'no';
+	protected $primaryKey = ['idNovel','no'];
 	const CREATED_AT = 'dateCreated';
 	const UPDATED_AT = 'dateRevision';
+	public $incrementing = false;
 
 	//Pseudo-column = addSelect(DB::raw('(textOriginal IS NOT NULL) as hasText'))
 
@@ -39,4 +41,43 @@ class Chapter extends Model
         $text = str_replace('[[', '', $text);
         return $text;
     }
+
+/**
+ * Set the keys for a save update query.
+ *
+ * @param  \Illuminate\Database\Eloquent\Builder  $query
+ * @return \Illuminate\Database\Eloquent\Builder
+ */
+protected function setKeysForSaveQuery(Builder $query)
+{
+    $keys = $this->getKeyName();
+    if(!is_array($keys)){
+        return parent::setKeysForSaveQuery($query);
+    }
+
+    foreach($keys as $keyName){
+        $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+    }
+
+    return $query;
+}
+
+/**
+ * Get the primary key value for a save query.
+ *
+ * @param mixed $keyName
+ * @return mixed
+ */
+protected function getKeyForSaveQuery($keyName = null)
+{
+    if(is_null($keyName)){
+        $keyName = $this->getKeyName();
+    }
+
+    if (isset($this->original[$keyName])) {
+        return $this->original[$keyName];
+    }
+
+    return $this->getAttribute($keyName);
+}
 }
