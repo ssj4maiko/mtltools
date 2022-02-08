935d10017af7cc040caaec231c045221fcb2cbae
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2022-02-08 08:47:16 -0300

    Trying to debug why novel form is not working on prod

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,6 @@
 <style>html,body{margin:0;padding:0}html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}html{background-color:#fff;font-size:16px;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;min-width:300px;overflow-x:hidden;overflow-y:scroll;text-rendering:optimizeLegibility;-webkit-text-size-adjust:100%;text-size-adjust:100%}body{font-family:BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif}body{color:#4a4a4a;font-size:1rem;font-weight:400;line-height:1.5}html{background-color:#000;color:#fff}</style><link rel="stylesheet" href="styles.90371f6dda376510.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.90371f6dda376510.css"></noscript></head>
 <body>
 	<app-root></app-root>
-<script src="runtime.257db61644338d90.js" type="module"></script><script src="polyfills.b59cc0e6a1181604.js" type="module"></script><script src="main.aaf2e6f5e43d6ac0.js" type="module"></script>
+<script src="runtime.8b7a052738f6d243.js" type="module"></script><script src="polyfills.b59cc0e6a1181604.js" type="module"></script><script src="main.aaf2e6f5e43d6ac0.js" type="module"></script>
 
 </body></html>
