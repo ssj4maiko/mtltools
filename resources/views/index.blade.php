0a9ed989d97a4fea1dd53339e8fe3b16ad0b73aa
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2022-02-07 23:35:24 -0300

    Compiling prod version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
+<!DOCTYPE html><html lang="en"><head>
 	<meta charset="utf-8">
 	<title>MTL Tools</title>
 	<base href="/">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles.css"></head>
+<style>html,body{margin:0;padding:0}html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}html{background-color:#fff;font-size:16px;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;min-width:300px;overflow-x:hidden;overflow-y:scroll;text-rendering:optimizeLegibility;-webkit-text-size-adjust:100%;text-size-adjust:100%}body{font-family:BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif}body{color:#4a4a4a;font-size:1rem;font-weight:400;line-height:1.5}html{background-color:#000;color:#fff}</style><link rel="stylesheet" href="styles.90371f6dda376510.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.90371f6dda376510.css"></noscript></head>
 <body>
 	<app-root></app-root>
-<script src="runtime.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="vendor.js" type="module"></script><script src="main.js" type="module"></script></body>
-</html>
+<script src="runtime.257db61644338d90.js" type="module"></script><script src="polyfills.b59cc0e6a1181604.js" type="module"></script><script src="main.fbeb8eafedef5c07.js" type="module"></script>
+
+</body></html>
