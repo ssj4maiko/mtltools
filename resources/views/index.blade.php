8e3b7d1f728c6c3c2d4d96e2aad1214f8425c1ff
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2022-05-24 17:40:38 -0300

    Compiled prod

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
+<!DOCTYPE html><html lang="en"><head>
 	<meta charset="utf-8">
 	<title>MTL Tools</title>
 	<base href="/">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles.css"></head>
+<style>@charset "UTF-8";html{background-color:#000;color:#fff}html,body{margin:0;padding:0}html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}html{background-color:#fff;font-size:16px;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;min-width:300px;overflow-x:hidden;overflow-y:scroll;text-rendering:optimizeLegibility;-webkit-text-size-adjust:100%;text-size-adjust:100%}body{font-family:BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif}body{color:#4a4a4a;font-size:1em;font-weight:400;line-height:1.5}html,body{margin:0;padding:0}html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}html{background-color:#000;font-size:16px;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;min-width:300px;overflow-x:hidden;overflow-y:scroll;text-rendering:optimizeLegibility;-webkit-text-size-adjust:100%;text-size-adjust:100%}body{font-family:BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif}body{color:#fff;font-size:1em;font-weight:400;line-height:1.5}</style><link rel="stylesheet" href="styles.5abdfb2d3fbf700b.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.5abdfb2d3fbf700b.css"></noscript></head>
 <body>
 	<app-root></app-root>
-<script src="runtime.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="vendor.js" type="module"></script><script src="main.js" type="module"></script></body>
-</html>
+<script src="runtime.684e9a7dc76ff14a.js" type="module"></script><script src="polyfills.99aaa26d0a2cad0c.js" type="module"></script><script src="main.08ae6dcd4cf29c7a.js" type="module"></script>
+
+</body></html>
