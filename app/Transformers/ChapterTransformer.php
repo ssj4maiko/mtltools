be18df42bb4a46dd8bdcb59528df708415be7346
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2022-07-25 15:46:13 -0300

    Adding missing Chapter Transformer too

--- app/Transformers/ChapterTransformer.php
+++ app/Transformers/ChapterTransformer.php
@@ -7,17 +7,35 @@ use App\Models\Chapter;
 
 class ChapterTransformer extends TransformerAbstract
 {
-	private $driver;
+    private $driver;
     public function __construct(?DriverInterface $driver)
-		if($chapter->textOriginal){
-			$base['textOriginal']	=	$chapter->textOriginal;
-		}
-		if($chapter->textRevision){
-			$base['textRevision']	=	$chapter->textRevision;
-		}
-		if($chapter->textCustom){
-			$base['textCustom']	=	$chapter->textCustom;
-		}
-		return $base;
-	}
+    {
+        $this->driver = $driver;
+    }
+    public function transform(Chapter $chapter)
+    {
+        $base = [
+            "idNovel"				=> $chapter->idNovel,
+            "no"					=> $chapter->no,
+            "arc"					=> $chapter->arc,
+            "title"					=> $chapter->title,
+            "dateOriginalPost"		=> $chapter->dateOriginalPost,
+            "dateOriginalRevision"	=> $chapter->dateOriginalRevision,
+            "dateCreated"			=> $chapter->dateCreated,
+            "dateRevision"			=> $chapter->dateRevision,
+            //"noCode"				=> $chapter->noCode,
+            "hasText"				=> $chapter->hasText,
+            "urlSource"				=> $chapter->getUrlSource($this->driver)
+        ];
+        if ($chapter->textOriginal) {
+            $base['textOriginal']	=	$chapter->textOriginal;
+        }
+        if ($chapter->textRevision) {
+            $base['textRevision']	=	$chapter->textRevision;
+        }
+        if ($chapter->textCustom) {
+            $base['textCustom']	=	$chapter->textCustom;
+        }
+        return $base;
+    }
 }
