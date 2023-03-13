27768ebbea8f6b4ac4bdbc03ce027bbc9f3cb393
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2023-03-13 17:19:32 -0300

    Trying to fix build-php script

--- .jenkins/docker-build-php.sh
+++ .jenkins/docker-build-php.sh
@@ -8,8 +8,11 @@ if [ -d "./resources/frontend" ]; then
     docker build -t $PHP_BUILD_NAME .
 
     cd ../../
-    if [ ! -f ".env"]; then
+    if [ ! -f .env ]; then
+        echo ".env file did not exist. Creating it from the example file."
         cp .env.example .env
+    else
+        echo ".env file already exists"
     fi
     docker run -v ./:/var/www/api -w /var/www/api --restart=no $PHP_BUILD_NAME bash -c "composer install; php artisan key:generate;"
 else
