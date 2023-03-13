c1f9bd78075a2fb0ea28e87bb08a936141959158
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2023-03-13 15:42:44 -0300

    Creating API KEY during laravel automated test.

--- .jenkins/docker-test-php.sh
+++ .jenkins/docker-test-php.sh
@@ -4,7 +4,7 @@ PHP_BUILD_NAME="docker-mtltools-php-api"
 
 ## Use these from root
 if [ -d "./resources/frontend" ]; then
-    docker run -v ./:/var/www/api -w /var/www/api --restart=no $PHP_BUILD_NAME php artisan test
+    docker run -v ./:/var/www/api -w /var/www/api --restart=no $PHP_BUILD_NAME bash -c "php artisan key:generate; php artisan test;"
 else
     echo "You must run this script from the project's root."
     exit 0
