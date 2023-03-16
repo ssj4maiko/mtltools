8531e34fd60814337e080ed434e6a782152e2236
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2023-03-16 17:39:06 -0300

    Deploy didn't work, testing some results

--- .jenkins/upload-files.sh
+++ .jenkins/upload-files.sh
@@ -3,10 +3,21 @@
 if [ -d "./resources/frontend" ]; then
     source .jenkins/variables.sh
 
+    echo "Create public2 folder"
     ssh -i $SSH_CREDENTIALS $SSH_USER@$SSH_HOST mkdir $SSH_REMOTE_DIRECTORY_BASE/public2
+    echo "ssh -i $SSH_CREDENTIALS $SSH_USER@$SSH_HOST mkdir $SSH_REMOTE_DIRECTORY_BASE/public2" | base64
+
+    echo "Upload everything from public to remote server"
     scp -i $SSH_CREDENTIALS -r public/* $SSH_USER@$SSH_HOST:$SSH_REMOTE_DIRECTORY_BASE/public2/
+    echo "scp -i $SSH_CREDENTIALS -r public/* $SSH_USER@$SSH_HOST:$SSH_REMOTE_DIRECTORY_BASE/public2/" | base64
+
+    #echo "Remove original public folder"
     #ssh -i $SSH_CREDENTIALS $SSH_USER@$SSH_HOST rm -R $SSH_REMOTE_DIRECTORY_BASE/public
+
+    #echo "Upload new home for updated scripts"
     #scp -i $SSH_CREDENTIALS -r resources/views/index.blade.php $SSH_USER@$SSH_HOST:$SSH_REMOTE_DIRECTORY_BASE/resources/views/index.blade.php
+
+    #echo "Rename `public2` to `public`"
     #ssh -i $SSH_CREDENTIALS $SSH_USER@$SSH_HOST mv $SSH_REMOTE_DIRECTORY_BASE/public2 $SSH_REMOTE_DIRECTORY_BASE/public
 else
     echo "You must run this script from the project's root."
