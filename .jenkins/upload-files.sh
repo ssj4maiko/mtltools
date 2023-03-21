77be3a84d0adb689ae733e688e9cbc8baa25a592
Maiko Gabriel Kinzel Engelke <me@maikoengelke.com>
2023-03-21 11:31:08 -0300

    fix commands to remove temporary folder on deploy

--- .jenkins/upload-files.sh
+++ .jenkins/upload-files.sh
@@ -9,17 +9,17 @@ if [ -d "./resources/frontend" ]; then
     ssh -i $SSH_CREDENTIALS $SSH_USER@$SSH_HOST <<REMOTE_COMMANDS
         cd $SSH_REMOTE_DIRECTORY_BASE;
 
-        git reset --hard HEAD
+        git reset --hard HEAD;
         git pull;
 
-        php composer.phar update
-        php artisan migrate --force
-        php artisan config:cache
-        php artisan route:cache
-        php artisan view:cache
-        php artisan event:cache
+        php composer.phar update;
+        php artisan migrate --force;
+        php artisan config:cache;
+        php artisan route:cache;
+        php artisan view:cache;
+        php artisan event:cache;
 
-        mkdir public_tmp
+        mkdir public_tmp;
 REMOTE_COMMANDS
 
     echo "Upload all compiled files to a temporary folder on remote server"
@@ -31,9 +31,10 @@ REMOTE_COMMANDS
 
     echo "Remove original public folder content, replace with new content, remove public_tmp folder"
     ssh -i $SSH_CREDENTIALS $SSH_USER@$SSH_HOST <<REMOTE_COMMANDS
-        rm -R $SSH_REMOTE_DIRECTORY_BASE/public/*
-        mv $SSH_REMOTE_DIRECTORY_BASE/public_tmp/* $SSH_REMOTE_DIRECTORY_BASE/public
-        rm public_tmp
+        cd $SSH_REMOTE_DIRECTORY_BASE;
+        rm -R public/*;
+        mv public_tmp/* public;
+        rm public_tmp;
 REMOTE_COMMANDS
 
     if [[ ! -z "$BRANCH_NAME" ]]; then
