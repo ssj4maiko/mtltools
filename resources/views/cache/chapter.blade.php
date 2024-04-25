171d9871027a61e552bab644a07759e9342ee552
Maiko Engelke <me@maikoengelke.com>
2024-04-24 22:41:33 -0300

    Adding support for <ruby> with text outside <rb>, also adding a "tooltip" for the source text to be selectable.

--- resources/views/cache/chapter.blade.php
+++ resources/views/cache/chapter.blade.php
@@ -79,6 +79,10 @@
         </pre>
     </div>
     <style type="text/css" rel="stylesheet">
+        #content-text {
+            display: none;
+        }
+
         #content-text>* {
             position: relative;
         }
@@ -100,50 +104,145 @@
         .duplicate {
             color: #99F;
         }
+        .tooltip {
+            position: absolute;
+            background-color: #333;
+            color: #fff;
+            padding: 5px;
+            border-radius: 3px;
+            font-size: 14px;
+            z-index: 9999; /* Ensure the tooltip appears above other elements */
+            --pointer-events: none; /* Allow mouse events to pass through the tooltip */
+        }
+
     </style>
     <script type='text/javascript'>
+        const allowedTags = ['P', 'DIV', 'H1', 'H2', 'H3'];
+        const contentContainer = document.querySelectorAll('#content-text *');
+
+        class HoverTooltip {
+            constructor(element) {
+                this.tooltip = null;
+                this.timeout = null;
+                this.element = element;
+                this.text = element.dataset.originalText;
+            }
+
+            createTooltip() {
+                const tooltip = document.createElement('div');
+                tooltip.classList.add('tooltip');
+                tooltip.textContent = this.text;
+                document.body.appendChild(tooltip);
+                return tooltip;
+            }
+
+            showTooltip() {
+                console.log('show Tooltip', this.timeout);
+                if (this.timeout) {
+                    clearTimeout(this.timeout); // Clear any existing timeout
+                } else {
+                    this.tooltip = this.createTooltip();
+                    this.updateTooltipPosition();
+
+                    // Add event listeners to handle mouse movements inside the tooltip
+                    this.tooltip.addEventListener('mouseenter', () => {
+                        clearTimeout(this.timeout); // Clear the hide timeout when mouse enters the tooltip
+                    });
+                    this.tooltip.addEventListener('mouseleave', () => {
+                        this.hideTooltipWithDelay(); // Restart the hide timeout when mouse leaves the tooltip
+                    });
+                }
+            }
+
+            updateTooltipPosition() {
+                const rect = this.element.getBoundingClientRect();
+                const left = rect.left + window.scrollX;
+                const top = rect.top + window.scrollY;
+                this.tooltip.style.left = left + 'px';
+                this.tooltip.style.top = top + 'px';
+            }
+            hideTooltipWithDelay() {
+                this.timeout = setTimeout(() => {
+                    this.hideTooltip();
+                }, 1500); // 3000 milliseconds (3 seconds)
+            }
+            hideTooltip() {
+                //console.log('Remove Tooltip', this.tooltip);
+                if (this.tooltip) {
+                    document.body.removeChild(this.tooltip);
+                    this.tooltip = null;
+                }
+                this.timeout = null;
+            }
+        }
         const RTTags = Array.from(document.querySelectorAll('RT')).map((item) => {
             item.textContent = '(' + item.textContent + ')';
         })
 
-        const allowedTags = ['P', 'DIV', 'H1', 'H2', 'H3'];
-        const contentContainer = document.querySelectorAll('#content-text *');
 
+        function removeTooltip(toolTip) {
+            //console.log('mouseLeave')
+            toolTip.hideTooltipWithDelay();
+            return toolTip;
+        }
+        function addTooltip(toolTip, element) {
+            //console.log('mouseEnter', element.dataset.originalText)
+            if(!toolTip)
+                toolTip = new HoverTooltip(element)
+
+            toolTip.showTooltip(element);
+            return toolTip;
+        }
+        function insertTooltip(element) {
+            let tooltip;
+            element.addEventListener('mouseenter', () => {
+                tooltip = addTooltip(tooltip, element);
+            });
+            element.addEventListener('mouseleave', () => {
+                tooltip = removeTooltip(tooltip);
+            });
+        }
         // Loop through child elements and process valid ones
         Array.from(contentContainer).forEach(element => {
-            if (allowedTags.includes(element.tagName)) {
-                const originalText = element.textContent.trim();
-                element.dataset.originalText = '| ' + originalText;
-
-                const rubyTags = element.querySelectorAll('ruby');
-                if (rubyTags.length > 0) {
-                    // Clone the original element
-                    const duplicateElement = element.cloneNode(true);
-                    // Iterate through each <ruby> tag
-                    rubyTags.forEach(ruby => {
-                        // Get the contents of <rb> and <span> tags
-                        const rbContent = ruby.querySelector('rb')?.textContent;
-                        const spanContents = Array.from(ruby.querySelectorAll('span')).map(span => span.textContent).join('');
-
-                        // Replace the <ruby> tag with the contents of <rb> and <span>
-                        duplicateElement.innerHTML = duplicateElement.innerHTML.replace(/<ruby>.*?<\/ruby>/, rbContent ?? spanContents);
-                    });
-                    // Insert the duplicated element after the current one
-                    duplicateElement.classList.add('duplicate');
-                    element.parentNode.insertBefore(duplicateElement, element.nextSibling);
+            if(element.textContent){
+                insertTooltip(element);
+                if (allowedTags.includes(element.tagName)) {
+                    const rubyTags = element.querySelectorAll('ruby');
+                    if (rubyTags.length > 0) {
+                        // Clone the original element
+                        const duplicateElement = element.cloneNode(true);
+                        // Iterate through each <ruby> tag
+                        rubyTags.forEach(ruby => {
+                            // Check if there are any <rp> tags and remove them
+                            Array.from(ruby.querySelectorAll('rp')).forEach(rp => rp.remove());
+    
+                            // Check if there are direct text nodes inside <ruby>
+                            const directTextNodes = Array.from(ruby.childNodes).filter(node => node.nodeType === Node.TEXT_NODE);
+                            // Extract text content from direct text nodes
+                            const directTextContent = directTextNodes.map(node => node.textContent).join('');
+                            // Get the contents of <rb> and <span> tags
+                            const rbContent = ruby.querySelector('rb')?.textContent;
+                            const spanContents = Array.from(ruby.querySelectorAll('span')).map(span => span.textContent).join('');
+    
+                            // Replace the <ruby> tag with the contents of <rb> and <span>
+                            duplicateElement.innerHTML = duplicateElement.innerHTML.replace(/<ruby>.*?<\/ruby>/, rbContent ? rbContent : (spanContents ? spanContents : directTextContent));
+                        });
+                        // Insert the duplicated element after the current one
+                        duplicateElement.classList.add('duplicate');
+                        duplicateElement.dataset.originalText = '| ' + duplicateElement.textContent.trim();
+                        insertTooltip(duplicateElement);
+                        element.parentNode.insertBefore(duplicateElement, element.nextSibling);
+                    }
+    
+                    const originalText = element.textContent.trim();
+                    element.dataset.originalText = '| ' + originalText;
                 }
+                window.setTimeout(() => {
+                    document.getElementById('content-text').style.display = 'inherit';
+                }, 500);
             }
         });
-        console.log(contentContainer);
     </script>
-    <!-- <script type='text/javascript'>
-        const translatedElements = Array.from(document.getElementById('content-text').children);
-        translatedElements.forEach(element => {
-            const originalText = element.textContent.trim();
-            element.dataset.originalText = '| ' + originalText;
-        });
-        console.log(translatedElements);
-    </script> -->
 </body>
 
 </html>
